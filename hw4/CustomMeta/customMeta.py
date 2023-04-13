7b084e69a1d97016d158159b7fdb1fa7a39fea69
Ilya Kekyashkiev <kekiashkiev.is19@physics.msu.ru>
2023-04-13 18:28:00 +0300

    CustomMeta was redone

--- hw4/CustomMeta/customMeta.py
+++ hw4/CustomMeta/customMeta.py
@@ -1,24 +1,35 @@
-""" Модуль, реализующий метакласс"""
+"""Модуль, добавляющий префикс custom"""
 
 
 class CustomMeta(type):
-    """ Добавляет префикс "custom_" в начале названий всех атрибутов
-     и методов, кроме магических """
+    """Реализация метакласса"""
+    def __setattr__(cls, key, value):
+        new_name = "custom_" + key
+        object.__setattr__(cls, new_name, value)
 
-    def __new__(cls, name, parents, attr):
-        """Создание нового экземпеляра класса"""
-        magic_methods = dict((name, val) for name, val in attr.items() if name.startswith('__')
-                             and name.endswith('__'))
-        custom = dict(('custom_' + name, val) for name, val in attr.items() if not name.startswith('__'))
-        new_attrs = {**custom, **magic_methods}
-        old_set_attr = object.__setattr__
-        new_attrs['old_set_attr'] = old_set_attr
+    def __new__(cls, name, bases, classdict):
+        attrs = ((name, value) for name, value in classdict.items() if
+                 not (name.startswith('__') and name.endswith('__')))
+        custom_attr = dict(("custom_" + name, value) for name, value in attrs)
+        attrs = ((name, value) for name, value in classdict.items() if
+                 (name.startswith('__')) and name.endswith('__'))
+        custom_attr.update(attrs)
+        custom_attr['__setattr__'] = cls.__setattr__
+        return super().__new__(cls, name, bases, custom_attr)
 
-        def set_custom_attr(self, name, value):
-            """ Вызывается при попытке присвоения атрибута """
-            new_name = name
-            if not name.startswith('__'):
-                new_name = 'custom_' + name
-            self.old_set_attr(new_name, value)
-            new_attrs['__setattr__'] = set_custom_attr
-            return super(CustomMeta, cls).__new__(cls, name, parents, new_attrs)
+
+class CustomClass(metaclass=CustomMeta):
+    x = 50
+
+    def __init__(self, val=99):
+        self.val = val
+
+    def line(self):
+        return 100
+
+    def __str__(self):
+        return "Custom_by_metaclass"
+
+
+Example = CustomClass()
+""" экземпляр CustomClass """
